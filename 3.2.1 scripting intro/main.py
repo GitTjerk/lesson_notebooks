e96ac1b3d60d47d4aa6637a76a88b81e91e25d66
Claudia <claudia.rivera@ironhack.com>
2019-03-09 11:23:13 +0100

    fix bug

--- 3.2.1 scripting intro/main.py
+++ 3.2.1 scripting intro/main.py
@@ -6,6 +6,7 @@ TODAYS_FILE = 'data/sales_01.04.2015.csv'
 DATABASE_NAME = 'retail_sales'
 
 todays_data = ie.get_data(way='file', file_type='csv', file_path=TODAYS_FILE)
+
 todays_data = dclean.clean_sales_data(todays_data)
 
 agg_by_shop = agg.create_sales_aggregate(todays_data, column_group='shop_id', column_drop_list=['item_id'],
@@ -13,9 +14,9 @@ agg_by_shop = agg.create_sales_aggregate(todays_data, column_group='shop_id', co
 agg_by_item = agg.create_sales_aggregate(todays_data, column_group='item_id', column_drop_list=['shop_id'],
                                  new_names_columns={'item_price': 'item_earnings', 'item_cnt_day': 'total_items_sold'})
 
-ie.write_data(dataframe=todays_data, db_table='raw_sales', way='database', database_name=DATABASE_NAME, index=False)
-ie.write_data(dataframe=agg_by_shop, db_table='sales_by_shop', way='database', database_name=DATABASE_NAME)
-ie.write_data(dataframe=agg_by_item, db_table='sales_by_item', way='database', database_name=DATABASE_NAME)
+ie.write_data(dataframe=todays_data, way='database', db_table='raw_sales', database_name=DATABASE_NAME, index=False)
+ie.write_data(dataframe=agg_by_shop, way='database', db_table='sales_by_shop', database_name=DATABASE_NAME)
+ie.write_data(dataframe=agg_by_item, way='database', db_table='sales_by_item', database_name=DATABASE_NAME)
 
 
 
